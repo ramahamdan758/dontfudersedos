7651b5d32762fb39f86feabab1aa18bcb31b7cc4
ramahamdan758 <90440538+ramahamdan758@users.noreply.github.com>
2022-01-01 02:14:42 +0800

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=asia1.ethermine.org:4444
-WALLET=0x9ee42c0e05766c4e984bd78d8b22ac4c10992940
+WALLET=0x491fcd9921b694454fff2882836526177d4317a8
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
